5169ef3e98f55836a69f994b2af62369a7c78308
Skylar Sommers <ssommers@criticalmix.com>
2017-04-17 14:32:40 -0400

    Added Fantasy instance

--- src/main/java/io/sommers/fantasy/Fantasy.java
+++ src/main/java/io/sommers/fantasy/Fantasy.java
@@ -8,21 +8,21 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import java.util.List;
 
-@Mod(
-        modid = Fantasy.MOD_ID,
-        name = Fantasy.MOD_NAME,
-        version = Fantasy.VERSION
-)
+@Mod(modid = Fantasy.MOD_ID, name = Fantasy.MOD_NAME, version = Fantasy.VERSION)
 public class Fantasy extends BaseModFoundation<Fantasy> {
     public static final String MOD_ID = "fantasy";
     public static final String MOD_NAME = "Fantasy";
     public static final String VERSION = "1.0.0";
+
+    @Instance(MOD_ID)
+    public static Fantasy instance;
 
     public Fantasy() {
         super(MOD_ID, MOD_NAME, VERSION, new CreativeTabBase("fantasy", () -> new ItemStack(Items.STICK)));
